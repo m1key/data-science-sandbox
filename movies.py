6b603840b8abd0b63e13c8ccbffa40e8abc453b5
Mike Huniewicz <michal.huniewicz@ft.com>
2015-07-07 15:05:42 +0100

    Read and print all data files.

--- movies.py
+++ movies.py
@@ -6,5 +6,12 @@ unames = ['user_id', 'gender', 'age', 'occupation', 'zip']
 users = pd.read_table(directory + 'users.dat', sep='::', header=None, names=unames, engine='python')
 
 rnames = ['user_id', 'movie_id', 'rating', 'timestamp']
-ratings = pd.read_table(directory + 'users.dat', sep='::', header=None, names=rnames, engine='python')
+ratings = pd.read_table(directory + 'ratings.dat', sep='::', header=None, names=rnames, engine='python')
+
+mnames = ['movie_id', 'title', 'genres']
+movies = pd.read_table(directory + 'movies.dat', sep='::', header=None, names=mnames, engine='python')
+
+print users[:5]
+print ratings[:5]
+print movies[:5]
 
