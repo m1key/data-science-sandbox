7a3c0754d7a0c2f1e475a288f5b09c60942d9ba0
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2015-10-01 10:32:43 +0100

    Loading American presidential data.

---
+++ presidential.py
@@ -0,0 +1,13 @@
+# http://www.fec.gov/disclosurep/PDownload.do
+# Using the all file from above.
+# P00000001-ALL.zip -> P00000001-ALL.csv
+
+import pandas as pd
+
+fec = pd.read_csv('P00000001-ALL.csv')
+#fec = pd.read_csv('../pydata-book/ch09/P00000001-ALL.csv')
+fec.info()
+print fec.ix[123456]
+unique_cands = fec.cand_nm.unique()
+print unique_cands
+
